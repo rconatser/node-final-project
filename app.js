a40a4cc37b7026cc3b0034d6ae218034d857474c
Rachel Conatser <rconatser7@gmail.com>
2019-11-30 18:45:41 -0700

    added new page styles mock website styles

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', function(req, res) { res.send('Hello World!') });
+app.get('/', function(req, res) { res.send('Page Loaded Unsuccessfully.') });
 app.listen(port, function() { console.log(`Example app listening on port ${port}!`) }); // back tick allows us to utilize js variables
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const formidable = require('formidable');
 const fs = require('fs');
 const path = require('path');
-const imagePath = path.join(__dirname, 'uploads/test.png');
+const imagePath = path.join(__dirname, '/uploads/test.png');
 
 const app = express();
 const port = 8080;
@@ -21,24 +21,46 @@ app.post('/upload', (req, res) => {
 
         res.send(`<!DOCTYPE html>
         <html lang="en">
+        
         <head>
-            <title>Form Response</title>
-            <link rel="stylesheet" href="styles.css" />
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+            <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+            <link rel="stylesheet" type="text/css" href="styles.css">
+            <title>Final Project | Web Languages II</title>
+            <nav>
+                <img src="images/logo.jpg" alt="logo" />
+                <a href="#contact">Contact</a>
+            </nav>
         </head>
-        <body class="home">
-           <div class="container">
-             <h1>Your Form Input Data</h1>
-             <h2>Your Image</h2>
-             <img src="/show" />
-             <h2>Received Data</h2>
-            <p>${ data }</p>
+        
+        <body>
+            <div class="header">
+                <div class="container">
+                    <h1>Submission Verification</h1>
+                    <p>Here are your responses</p>
+                </div>
             </div>
+            <div class="section response">
+                <h2>Your Response</h2>
+                <h3>Your Name:</h3>
+                <p>${data}</p>
+
+                <h3>Your Image:</h3>
+                <img src="/show" alt="Uploaded image" />               
+            </div>
+            <footer>
+                &copy; 2019 Rachel Conatser | DGM 3760 | Final Project
+            </footer>
         </body>
+        
         </html>`);
     });
 });
 
-app.get('/show', (req, res) => {
+app.get('/show', (res) => {
     console.log('Request handler "show" was called.');
     res.sendFile(imagePath);
 });
